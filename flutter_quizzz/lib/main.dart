9098e057c8dca106b0c21394beb6fdc23c05d045
ramziChammem <ramzichammem69@gmail.com>
2021-05-04 03:49:05 +0100

    prepared the appBart and basic body layout

--- flutter_quizzz/lib/main.dart
+++ flutter_quizzz/lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    var questions = [
+      "what's your favourite color?",
+      "what's your favourite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Quizz"),
+        ),
+        body: Column(
+          children: [
+            Text("The question"),
+            RaisedButton(child: Text("Answer 1"), onPressed: null),
+            RaisedButton(child: Text("Answer 2"), onPressed: null),
+            RaisedButton(child: Text("Answer 3"), onPressed: null),
+
+            ],
+        ),
+      ),
+    );
   }
 }
